fbd5f53c147869714775d030dbae6f60e896123b
Achal Chauhan <43654070+AchalChauhan@users.noreply.github.com>
2024-03-18 23:18:20 +0530

    Positonal changes to pass the testcases

--- Play/src/Components/Logo/Logo.component.jsx
+++ Play/src/Components/Logo/Logo.component.jsx
@@ -1,6 +1,7 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 import logo from "../../assets/Logo.png"
+import './Logo.styles.css'
 const Logo = () => {
   return (
     <>

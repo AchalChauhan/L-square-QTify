e35264ee013fd9e5f8f2eba4ddacdc29dec9cd85
Achal Chauhan <43654070+AchalChauhan@users.noreply.github.com>
2024-04-01 07:11:40 +0530

    nav Debug ip

--- Play/src/Components/Button/Button.jsx
+++ Play/src/Components/Button/Button.jsx
@@ -1,4 +1,4 @@
-// import  from "react";
+import React from "react";
 import styles from "./Button.module.css";
 const Button = ({ children }) => {
   return <button className={styles.button}>{children}</button>;
--- Play/src/Components/NavBar/Navbar.jsx
+++ Play/src/Components/NavBar/Navbar.jsx
@@ -1,6 +1,4 @@
-/* eslint-disable react/no-children-prop */
-/* eslint-disable react/prop-types */
-// import React from "react";
+import React from "react";
 import Button from "../Button/Button";
 import Logo from "../Logo/Logo";
 import styles from "./NavBar.module.css";

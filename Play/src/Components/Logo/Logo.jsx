b56d215144d393238af0903c7ca525aefd88d7ca
Achal Chauhan <43654070+AchalChauhan@users.noreply.github.com>
2024-04-01 07:01:47 +0530

    ip

--- Play/src/Components/Logo/Logo.jsx
+++ Play/src/Components/Logo/Logo.jsx
@@ -2,7 +2,7 @@
 import Qtify from "../../assets/Q tify.png";
 
 const Logo = () => {
-  return <img src={Qtify} alt='Logo' width='67px' height='34px' style={{paddingLeft : "5px"}}></img>;
+  return <img src={Qtify} alt='Logo' width='67px' height='34px' style={{paddingLeft : "5px", marginLeft: "5px"}}></img>;
 };
 
 export default Logo;
